03b427262001ac7df993a7ed6cf0bcf8a67af2a0
Jatin Varlyani <varlyanijatin88@gmail.com>
2018-10-20 13:33:37 +0530

    Initial Commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,8 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+       
+        <link rel="stylesheet" href="https://bootswatch.com/4/materia/bootstrap.min.css">
         <link rel="stylesheet" href="{{asset('css/app.css')}}">
         <title>{{config('app.name', 'Blog')}}</title>
 
